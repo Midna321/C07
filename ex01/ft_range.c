41970571e136dad1a1f140c6c1f49dc6694cf306
root <root@DESKTOP-G5GKQ09>
2024-07-24 19:56:58 +0200

    ex01 C07

---
+++ ex01/ft_range.c
@@ -0,0 +1,53 @@
+#include <stdlib.h>
+#include <stddef.h>
+#include <stdio.h>
+
+int *ft_range(int min, int max)
+{
+	int j = 0;
+	int range;
+	int *numarray;
+	if(min > max)
+	{
+		return NULL;
+	}
+	range = max - min;
+	numarray=(int*)malloc(sizeof(int) * (range));
+	if(numarray == NULL)
+	{
+		printf("Memory allocation failed");
+		return NULL;
+	}
+	while(min < max)
+	{
+		numarray[j] = min;
+		j++;
+		min++;
+	}
+	return numarray;
+}
+
+
+int main()
+{
+	int min = 1;
+	int max = 99;
+	int *numarray = ft_range(min, max);
+	int size = max - min;
+	printf("Array size : %i\n",size);
+	int i = 0;
+	while( i < size)
+	{
+		printf("Ad index %i corrisponde %i\n",i,numarray[i]);
+		i++;
+	}
+	return 0;
+}
+
+
+
+
+
+
+
+
